972728aac48c2577f0e09371e62f9a77086a12ff
Rodrigo <160552648+RodrigoB093@users.noreply.github.com>
2024-12-13 20:20:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random
 caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 longitud = int(input("Longitud de la contraseña:"))
-contraseña = ""
+contrasena = ""
 
 for i in range(longitud):
-    contraseña += caracteres[random.randint(0,71)]
+    contrasena += caracteres[random.randint(0,71)]
 
-print("Contraseña generada:", contraseña)
+print("Contraseña generada:", contrasena)
